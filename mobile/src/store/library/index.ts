0f4155d4080a87e952e3249b72d7ce5317378383
Adam Szi <szi.adam@simonyi.bme.hu>
2022-09-04 16:54:40 +0200

    AssetWatcher

--- mobile/src/store/library/index.ts
+++ mobile/src/store/library/index.ts
@@ -4,9 +4,12 @@ import { LibraryState } from './types';
 import { initLoadable } from '../../utils/loadableData';
 import { fetchTracks } from './thunk.actions';
 import { Track } from '../../types/model';
+import { AssetWatcher } from './helpers';
 
 const initialState: LibraryState = {
   activeTab: 'Tracks',
+  includedFolders: ['file:///storage/emulated/0/Music/'],
+  excludedFolders: ['file:///storage/emulated/0/Android/media/'],
   tracks: initLoadable({}),
   albums: initLoadable({}),
   artists: initLoadable({}),
@@ -24,7 +27,10 @@ export const librarySlice = createSlice({
 
     builder.addCase(fetchTracks.fulfilled, (state, { payload: assets }) => {
       state.tracks.status = 'succeeded';
+
+      const assetWatcher = new AssetWatcher(state.includedFolders, state.excludedFolders);
       state.tracks.data = assets.reduce((map: Record<string, Track>, asset) => {
+        if (!assetWatcher.isWatched(asset)) return map;
         return {
           ...map,
           [asset.id]: Track.from(asset),
