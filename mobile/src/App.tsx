0398639eb7ac2d628c31312d6f9168d9a69ab827
Adam Szi <szi.adam@simonyi.bme.hu>
2022-09-08 00:27:30 +0200

    wip # track player

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
-import { StatusBar, StyleSheet, View } from 'react-native';
+import { Text, StatusBar, StyleSheet, View } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 
 import { AppNavigator } from './screens/App.navigator';
 import { store } from './store';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    TrackPlayer.setupPlayer().finally(() => setLoading(false));
     StatusBar.setBarStyle('light-content');
   }, []);
 
   return (
     <View style={styles.container}>
-      <Provider store={store}>
-        <AppNavigator />
-      </Provider>
+      {loading && <Text>Player loading...</Text>}
+      {!loading && (
+        <Provider store={store}>
+          <AppNavigator />
+        </Provider>
+      )}
     </View>
   );
 }
