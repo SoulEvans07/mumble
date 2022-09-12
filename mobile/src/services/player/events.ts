798dad1e3de8f2fab71b695c877d71228581e9fb
Adam Szi <szi.adam@simonyi.bme.hu>
2022-09-12 21:13:56 +0200

    track events dummy

--- mobile/src/services/player/events.ts
+++ mobile/src/services/player/events.ts
@@ -1,1 +1,28 @@
-export default async function () {}
+import TrackPlayer, { Event } from 'react-native-track-player';
+
+export default async function () {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    console.log('remote-play');
+    TrackPlayer.play();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    console.log('remote-pause');
+    TrackPlayer.pause();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    console.log('remote-next');
+    TrackPlayer.skipToNext();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    console.log('remote-previous');
+    TrackPlayer.skipToPrevious();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteStop, () => {
+    console.log('remote-stop');
+    TrackPlayer.removeUpcomingTracks();
+  });
+}
